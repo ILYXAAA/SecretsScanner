4cb7b3143a6aeb2e32bc9bce1f249662ab0dd2c1
ILYXAAA <ilyagolybnichev@gmail.com>
2025-06-22 17:41:14 +0300

    Update admin

--- run.py
+++ run.py
@@ -260,7 +260,7 @@ def validate_environment():
 
     check_and_setup_user_database()
 
-    required_files = ["templates/dashboard.html", "templates/login.html",
+    required_files = ["templates/dashboard.html", "templates/login.html", "templates/admin.html",
                       "templates/multi_scan.html", "templates/project.html", "templates/scan_results.html", 
                       "templates/scan_status.html", "templates/settings.html", "utils/html_report_generator.py", "CredsManager.py", "main.py"]
     
